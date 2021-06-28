d4cea5cd17d06715c28e0f55868e63f4beb9090a
Menake Ratnayake <Menake.Ratnayake@beca.com>
2021-06-28 22:29:46 +1200

    Update card resizing

--- components/cards/ExpandedCard.js
+++ components/cards/ExpandedCard.js
@@ -41,7 +41,7 @@ const ExpandedCard = ({ canRsvp, eventName, src, date, guestEventId, hasRsvpd, i
                 <input hidden defaultChecked={guestsArray.length > 1 ? selectedNumberOfGuests : 1} />
                 {guestsArray.length > 1
                     ? (
-                        <div className="flex flex-row items-center justify-evenly mb-10">
+                        <div className="flex flex-row items-center justify-evenly mb-10 mt-10">
                             <h1 className="font-semibold w-2/5 lg:w-1/5 xl:w-2/5 text-primary text-left lg:text-left">Number of guests: </h1>
                             <AnimateSharedLayout type="crossfade" transition={{ duration: 0.1 }}>
                                 <ul className="w-3/5 lg:w-4/5 xl:w-3/5 flex flex-row flex-wrap justify-evenly">
@@ -65,36 +65,66 @@ const ExpandedCard = ({ canRsvp, eventName, src, date, guestEventId, hasRsvpd, i
     }
 
     return (
-        <motion.div className="h-screen w-screen absolute z-30 bg-white sm:min-h-20">
-            <div className="h-full w-full">
-                <div className="fixed top-0 h-full sm:h-4/5 xl:h-5/6 w-full sm:w-4/5 2xl:w-1/2 sm:inset-y-1/20 sm:inset-x-1/10 2xl:inset-x-1/4 m-auto rounded-md">
-                    <CardPhoto src={src} heading={eventName} className="h-1/3 lg:h-2/5 lg:rounded-t-lg" onClick={onClick} objectPosition={objectPosition} />
-                    <motion.div className="h-2/3 lg:h-3/5 px-10 shadow-md lg:rounded-b-lg"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ delay: 0.3, duration: 0.2 }} >
-                        <div className="relative h-full">
-                            <h1 className="pt-10 relative text-center text-lg 2xl:text-4xl uppercase font-semibold text-secondary sm:mb-10">{date}</h1>
-                            <div className="relative flex flex-row mt-2 2xl:text-lg">
-                                <h1 className="mr-4 font-semibold text-primary">Where: </h1>
-                                <p>{location}</p>
-                            </div>
-                            <div className="mt-5 xl:mt-10 2xl:text-lg">
-                                {blurb}
-                            </div>
-                            {
-                                showRsvpForm
-                                    ? <RsvpForm />
-                                    : <div className="absolute bottom-10 w-full italic">
-                                        You have already Rsvpd for this event. If you think this is an error or the number of guests attending has changed please get in touch with Sachintha or Pooja.
-                                    </div>
-                            }
+        <div className="absolute z-30 sm:bg-white inset-0">
+            <div className="fixed top-0 bottom-0 h-full w-full bg-white rounded-md overflow-auto shadow-md sm:bottom-auto sm:h-4/5 xl:h-5/6 w-full sm:w-4/5 2xl:w-1/2 sm:inset-y-1/20 sm:inset-x-1/10 2xl:inset-x-1/4">
+                <CardPhoto src={src} heading={eventName} className="h-1/3 lg:h-2/5 lg:rounded-t-lg" onClick={onClick} objectPosition={objectPosition} />
+                <motion.div className="h-2/3 min-h-[550px] sm:min-h-[400px] lg:h-3/5 px-10 shadow-md lg:rounded-b-lg"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ delay: 0.3, duration: 0.2 }} >
+                    <div className="relative h-full">
+                        <h1 className="pt-10 relative text-center text-lg 2xl:text-4xl uppercase font-semibold text-secondary sm:mb-10">{date}</h1>
+                        <div className="relative flex flex-row mt-2 2xl:text-lg">
+                            <h1 className="mr-4 font-semibold text-primary">Where: </h1>
+                            <p>{location}</p>
                         </div>
-                    </motion.div>
-                </div>
+                        <div className="mt-5 xl:mt-10 2xl:text-lg">
+                            {blurb}
+                        </div>
+                        {
+                            showRsvpForm
+                                ? <RsvpForm />
+                                : <div className="absolute bottom-10 w-full italic">
+                                    You have already Rsvpd for this event. If you think this is an error or the number of guests attending has changed please get in touch with Sachintha or Pooja.
+                                </div>
+                        }
+                    </div>
+                </motion.div>
             </div>
-        </motion.div>
+        </div>
     )
 }
 
 export default ExpandedCard
+
+
+
+        // <motion.div className="h-full min-h-[600px] w-screen absolute z-30 bg-white">
+        //     <div className="fixed top-0 bottom-0 h-full w-full bg-white z-30 sm:inset-y-1/20 sm:inset-x-1/10 2xl:inset-x-1/4 m-auto rounded-md overflow-auto ">
+        //         <CardPhoto src={src} heading={eventName} className="h-1/3 lg:h-2/5 lg:rounded-t-lg" onClick={onClick} objectPosition={objectPosition} />
+        //         <div className="bg-white h-2/3 min-h-[400px]">
+        //             <motion.div className="h-2/3 min-h-[550px] lg:h-3/5 px-10 shadow-md lg:rounded-b-lg"
+        //                 initial={{ opacity: 0 }}
+        //                 animate={{ opacity: 1 }}
+        //                 transition={{ delay: 0.3, duration: 0.2 }} >
+        //                 <div className="relative h-full">
+        //                     <h1 className="pt-10 relative text-center text-lg 2xl:text-4xl uppercase font-semibold text-secondary sm:mb-10">{date}</h1>
+        //                     <div className="relative flex flex-row mt-2 2xl:text-lg">
+        //                         <h1 className="mr-4 font-semibold text-primary">Where: </h1>
+        //                         <p>{location}</p>
+        //                     </div>
+        //                     <div className="mt-5 xl:mt-10 2xl:text-lg">
+        //                         {blurb}
+        //                     </div>
+        //                     {
+        //                         showRsvpForm
+        //                             ? <RsvpForm />
+        //                             : <div className="absolute bottom-10 w-full italic">
+        //                                 You have already Rsvpd for this event. If you think this is an error or the number of guests attending has changed please get in touch with Sachintha or Pooja.
+        //                             </div>
+        //                     }
+        //                 </div>
+        //             </motion.div>
+        //         </div>
+        //     </div>
+        // </motion.div>
