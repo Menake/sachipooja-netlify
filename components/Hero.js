16c1b0adbfa2527115201ba4b21b9ec7ea2cbf73
Menake Ratnayake <Menake.Ratnayake@beca.com>
2021-11-08 17:08:30 +1300

    Change date from nov21 to april 22

--- components/Hero.js
+++ components/Hero.js
@@ -31,7 +31,7 @@ const Hero = () => {
                     }}
                 >
                     <div className="absolute bottom-10 right-10 text-2xl md:text-6xl font-bold text-secondary float-right">
-                        Nov<span className="text-primary">'21</span>
+                        April<span className="text-primary">'22</span>
                     </div>
                 </motion.div>
                 <motion.div
